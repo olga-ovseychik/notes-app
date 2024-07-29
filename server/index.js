704a5626f39f120b00d2379271bc788d555d78ae
Olga Morozova <olga.ovsejchik@gmail.com>
2024-07-29 10:52:05 +0200

    4 commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",   
+    origin: "notes-app-frontend-theta.vercel.app", // http://localhost:5173
     credentials: true
 };
 
