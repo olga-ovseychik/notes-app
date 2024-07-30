8f0972bd6d6b982ed5e6c686e01c28715099cac5
Olga Morozova <olga.ovsejchik@gmail.com>
2024-07-30 12:05:15 +0200

    update profile 2

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -170,9 +170,8 @@ const Profile = () => {
             <div className="invisible text-xs text-slate-500 pl-2">All changes saved</div>
         </div>
         <div className='flex justify-end sm:gap-6 xs:flex-col xs:gap-3'>
-            <button 
-                type='submit'
-                className='sm:w-1/4 xs:w-full h-10 rounded-md bg-mainColor hover:bg-amber-200 text-textColor shadow-lg self-end'>{isLoading ? 'Loading...' : null} {isUpdateProfileSuccess ? 'Save' : null}</button>
+            <div>{isUpdateProfileSuccess ? 'Profile successfully updated' : null}</div>
+            <button type='submit' className='sm:w-1/4 xs:w-full h-10 rounded-md bg-mainColor hover:bg-amber-200 text-textColor shadow-lg self-end'>Save</button>
             <button onClick={onAccountDelete} className='sm:w-1/4 xs:w-full h-10 rounded-md bg-red-100 hover:bg-red-500 text-red-500 hover:text-white shadow-lg self-end'>Delete account</button>
         </div>
     </form>
