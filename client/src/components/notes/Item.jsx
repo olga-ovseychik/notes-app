48659c2dad966ed51cfc4cd5b4fdb88592d3a67d
Olga Morozova <olga.ovsejchik@gmail.com>
2024-07-30 11:46:15 +0200

    search 5

--- client/src/components/notes/Item.jsx
+++ client/src/components/notes/Item.jsx
@@ -21,12 +21,12 @@ const Item = ({ noteId, text, title, updatedAt, todos, task }) => {
             <div className={`xs:text-base break-words ${!title ? 'text-secTxtColor dark:text-semiLight' : 'text-hoverColor dark:text-zinc-200'}`}>
                 {title ? `${title.length > 30 ? `${title.slice(0, 30)}...` : title}` : 'Untitled'} 
             </div>
-            <div className={`text-sm break-words ${!text ? 'text-secTxtColor dark:text-semiLight' : 'text-hoverColor dark:text-zinc-200'}`}>
+            <div className={`ignore text-sm break-words ${!text ? 'text-secTxtColor dark:text-semiLight' : 'text-hoverColor dark:text-zinc-200'}`}>
                 {!text && todos.length == 0 && !task ? `Just start writing something` : `${text.length > 40 ? `${text.slice(0, 40)}...` : text}`}
                 {!text && task && todos.length == 0 ? `Type some todos` : null}
             </div>
             {todos.length > 0 ?
-                 <div>
+                 <div className="ignore">
                     <div className="inline-block relative py-2">
                         <span 
                             className="bg-mainColor dark:bg-mainColor text-textColor text-xs font-medium me-2 px-1.5 py-0.5 rounded text-center">
