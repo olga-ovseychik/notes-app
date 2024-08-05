cf6a76ce5ae160e47cbedd1e7c58641c90d0560e
Olga Morozova <olga.ovsejchik@gmail.com>
2024-08-05 14:16:36 +0200

    login 6

--- client/src/components/Load.jsx
+++ client/src/components/Load.jsx
@@ -1,6 +1,6 @@
 const Load = ({size, color}) => {
     return (
-        <div className="flex justify-center items-center absolute w-full h-full blur-2xl bg-white">
+        <div className="flex justify-center items-center absolute w-full h-full bg-white">
             <div
                 className={`inline-block h-${size} w-${size} animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] text-${color}`}
                 role="status">
